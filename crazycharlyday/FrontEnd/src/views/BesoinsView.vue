cad1888944e12d35e6f80cf781e645108d8736d1
TyrYoxan <clem.bri25@gmail.com>
2025-02-13 22:08:09 +0100

    Fix

--- crazycharlyday/FrontEnd/src/views/BesoinsView.vue
+++ crazycharlyday/FrontEnd/src/views/BesoinsView.vue
@@ -33,7 +33,7 @@ export default {
                 }
             };
 
-            axios.get('http://localhost:8080/besoins', config)
+            axios.get('http://docketu.iutnc.univ-lorraine.fr:60080/besoins', config)
                 .then(response => {
                     this.besoins = response.data;
                     if (this.besoins.length === 0) {
